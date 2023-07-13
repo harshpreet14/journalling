4d7f01a85cb00b059593f5bd59ac8dc06752335c
harshpreet14 <kaurharshpt6@gmail.com>
2023-07-13 22:48:55 +0530

    Database Schemas and Models

--- backend/controllers/entryController.js
+++ backend/controllers/entryController.js
@@ -1,37 +1,144 @@
+const Entry = require('./../models/entryModel');
 
+//a specific entry for a logged-in user
+exports.getEntry = async (req, res) => {
+    const { entry_id } = req.params;
+    const user_id = req.user._id; 
+  
+    try {
+      const entry = await Entry.findOne({ _id: entry_id, user: user_id });
+      if (!entry) {
+        return res.status(404).json({
+          status: 'fail',
+          message: 'Entry not found',
+        });
+      }
+      res.status(200).json({
+        status: 'success',
+        data: {
+          entry,
+        },
+      });
+    } catch (err) {
+      res.status(500).json({
+        status: 'error',
+        message: 'Internal server error',
+      });
+    }
+  };
+  
+//updating entry 
+exports.updateEntry = async (req, res) => {
+    const { entry_id } = req.params;
+    const user_id = req.user._id; // Assuming user ID is available in the req.user object
 
-exports.getAllEntries = (req,res) =>{
-    res.status(200).json({
+    try {
+        const updatedEntry = await Entry.findOneAndUpdate(
+        { _id: entry_id, user: user_id },
+        req.body,
+        { new: true, runValidators: true }
+        );
+        if (!updatedEntry) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Entry not found',
+        });
+        }
+        res.status(200).json({
+        status: 'success',
+        data: {
+            entry: updatedEntry,
+        },
+        });
+    } catch (err) {
+        res.status(400).json({
+        status: 'fail',
+        message: 'Invalid data sent!',
+        });
+    }
+};
+
+// deleting entry 
+exports.deleteEntry = async (req, res) => {
+    const { entry_id } = req.params;
+    const user_id = req.user._id; // Assuming user ID is available in the req.user object
+
+    try {
+        const deletedEntry = await Entry.findOneAndDelete({
+        _id: entry_id,
+        user: user_id,
+        });
+        if (!deletedEntry) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Entry not found',
+        });
+        }
+        res.status(204).json({
+        status: 'success',
+        data: null,
+        });
+    } catch (err) {
+        res.status(500).json({
+        status: 'error',
+        message: 'Internal server error',
+        });
+    }
+};
+
+exports.getAllEntries = async (req, res) => {
+    const { user_id } = req.user; // Assuming user ID is available in the req.user object
+
+    try {
+        const entries = await Entry.find({ user: user_id });
+        res.status(200).json({
         status: 'success',
         results: entries.length,
         data: {
-            entries
-        }
-    })
+            entries,
+        },
+        });
+    } catch (err) {
+        res.status(500).json({
+        status: 'error',
+        message: 'Internal server error',
+        });
+    }
 };
 
-exports.deleteAllEntries = (req,res) => {
+exports.deleteAllEntries = async (req, res) => {
+    const { user_id } = req.user; // Assuming user ID is available in the req.user object
+  
+    try {
+      await Entry.deleteMany({ user: user_id });
+      res.status(204).json({
+        status: 'success',
+        data: null,
+      });
+    } catch (err) {
+      res.status(500).json({
+        status: 'error',
+        message: 'Internal server error',
+      });
+    }
+  };
 
-}
-
-exports.getEntry = (req, res)=>{
-
-    res.status(200).json({
-        status:'success'
-    })
-}
-
-exports.createEntry = (req, res)=>{
-
-    res.status(201).json({
-        status:'success'
-    })
-}
-
-exports.updateEntry = (req,res)=> {
-
-}
-
-exports.deleteEntry = (req,res)=>{
-
-}
+exports.createEntry = async (req, res) => {
+    const { user_id } = req.user; // Assuming user ID is available in the req.user object
+  
+    try {
+      const newEntry = await Entry.create({ ...req.body, user: user_id });
+      res.status(201).json({
+        status: 'success',
+        data: {
+          entry: newEntry,
+        },
+      });
+    } catch (err) {
+      res.status(400).json({
+        status: 'fail',
+        message: 'Invalid data sent!',
+      });
+    }
+  };
+  
--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,20 +1,112 @@
+const User = require('./models/user');
 
-exports.getAllUsers = (req,res)=>{
-    res.status(200).send('Route not yet implemented.')
-}
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json({
+      status: 'success',
+      results: users.length,
+      data: {
+        users,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+  }
+};
 
-exports.createUser = (req,res)=>{
-    res.status(200).send('Route not yet implemented.')
-}
+exports.createUser = async (req, res) => {
+  try {
+    const newUser = await User.create(req.body);
+    res.status(201).json({
+      status: 'success',
+      data: {
+        user: newUser,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid data sent!',
+    });
+  }
+};
 
-exports.getUser = (req,res)=>{
-    res.status(200).send('Route not yet implemented.')
-}
+exports.getUser = async (req, res) => {
+  const { user_id } = req.body;
 
-exports.updateUser = (req,res)=>{
-    res.status(200).send('Route not yet implemented.')
-}
+  try {
+    const user = await User.findById(user_id);
+    if (!user) {
+      return res.status(404).json({
+        status: 'fail',
+        message: 'User not found',
+      });
+    }
+    res.status(200).json({
+      status: 'success',
+      data: {
+        user,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+  }
+};
 
-exports.deleteUser = (req,res)=>{
-    res.status(200).send('Route not yet implemented.')
-}
+exports.updateUser = async (req, res) => {
+  const { user_id } = req.body;
+
+  try {
+    const updatedUser = await User.findByIdAndUpdate(user_id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!updatedUser) {
+      return res.status(404).json({
+        status: 'fail',
+        message: 'User not found',
+      });
+    }
+    res.status(200).json({
+      status: 'success',
+      data: {
+        user: updatedUser,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid data sent!',
+    });
+  }
+};
+
+exports.deleteUser = async (req, res) => {
+  const { user_id } = req.body;
+
+  try {
+    const deletedUser = await User.findByIdAndDelete(user_id);
+    if (!deletedUser) {
+      return res.status(404).json({
+        status: 'fail',
+        message: 'User not found',
+      });
+    }
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+  }
+};

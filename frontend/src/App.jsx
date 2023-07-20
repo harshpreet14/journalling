fcc5d0664bfe9ae22bd2dca4887d6a81c86d1edf
harshpreet14 <kaurharshpt6@gmail.com>
2023-07-20 21:05:24 +0530

    Placing the Audio Component, latest

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,14 @@
 import { Landing, Profile, Dashboard } from "./pages";
-import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+//import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route exact path='/' > <Landing/> </Route>
-        <Route exact path='/me'><Dashboard/></Route>
-        <Route exact path='/profile'><Profile/></Route>
-      </Routes>
-    </Router>
+    <div>
+      <Landing/> 
+        <Dashboard/>
+        <Profile/>
+    </div>
+     
   );
 };
 
--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,13 +1,10 @@
 //import AudioRecording from './AudioRecording'
 import { useAuth0 } from "@auth0/auth0-react";
 import { useEffect } from "react";
-
+import Audio from "./Audio";
 
 const Sidebar = () => {
-
-  const {user} = useAuth0();
-  
-  
+  //const { user } = useAuth0();
 
   return (
     <div className="flex h-screen bg-[#FFF9BA]">
@@ -22,10 +19,10 @@ const SidebarNav = () => {
     <div className="w-80 bg-white p-6 ml-10 rounded-2xl border-2 border-black mt-20 mb-10 ">
       {/* Sidebar content */}
       <div className="mb-4">
-          <button className='bg-[#FFC000] ml-20 hover:bg-yellow-500 text-white  font-bold py-3 px-6 rounded-2xl '>
-            New Entry
-          </button>
-        </div>
+        <button className="bg-[#FFC000] ml-20 hover:bg-yellow-500 text-white  font-bold py-3 px-6 rounded-2xl ">
+          New Entry
+        </button>
+      </div>
       <ul className="space-y-2">
         <SidebarItem text="Item 1" />
         <SidebarItem text="Item 2" />
@@ -35,32 +32,33 @@ const SidebarNav = () => {
   );
 };
 
-const SidebarItem = ({text}) => {
+const SidebarItem = ({ text }) => {
   return (
     <li className="w-50 bg-white p-3 ml-15 rounded-2xl border border-black mt-3 mb-3 border-opacity-50">
-      <a href="#" className="text-black hover:text-gray-700">{text}</a>
+      <a href="#" className="text-black hover:text-gray-700">
+        {text}
+      </a>
     </li>
   );
 };
 
 const MainContent = () => {
-    return (
-      <div className="flex-grow w-1100 bg-white p-6 ml-10 mr-10 rounded-2xl border-2 border-black mt-20 mb-10 flex flex-col justify-between">
-        {/* Main content */}
-        <div>
-          <h1 className="text-gray-800 text-2xl font-bold mb-4">You Entry here...</h1>
-          <p className="text-gray-800">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </div>
-        <div className="flex items-center justify-center mt-6">
-       
-        </div>
+  return (
+    <div className="flex-grow w-1100 bg-white p-6 ml-10 mr-10 rounded-2xl border-2 border-black mt-20 mb-10 flex flex-col justify-between">
+      {/* Main content */}
+      <div>
+        <h1 className="text-gray-800 text-2xl font-bold mb-4">
+          You Entry here...
+        </h1>
+        <p className="text-gray-800">
+          Hi from main content 
+        </p>
       </div>
-    );
-  };
-  
-    
+      <div className="flex items-center justify-center mt-6">
+        <Audio/>
+      </div>
+    </div>
+  );
+};
+
 export default Sidebar;
-
-
-
-

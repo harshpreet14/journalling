888d59f0da1a4dcc33e19e7c38195c934428caec
harshpreet14 <kaurharshpt6@gmail.com>
2023-07-23 14:36:12 +0530

    Sidebar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -5,22 +5,18 @@ import Audio from "./Audio";
 import axios from "axios";
 
 
-
 const Sidebar = () => {
-  const {isAuthenticated} = useAuth0();
-  
-    return (
-      <div className="flex h-screen bg-[#FFF9BA]">
-        <SidebarNav />
-        <MainContent />
-      </div>
-    );
-  
- 
+  const { isAuthenticated } = useAuth0();
+
+  return (
+    <div className="flex h-screen bg-[#FFF9BA]">
+      <SidebarNav />
+      <MainContent />
+    </div>
+  );
 };
 
 const SidebarNav = () => {
-
   const { user, getAccessTokenSilently } = useAuth0();
   const [entries, setEntries] = useState([]);
 
@@ -29,23 +25,26 @@ const SidebarNav = () => {
     const fetchUserEntries = async () => {
       try {
         const token = await getAccessTokenSilently();
-        const response = await fetch('http://localhost:3000/journal-api/v1/entries', {
-          method: 'GET',
-          headers: {
-            Authorization: `Bearer ${token}`,
-            user_id: user.sub
-          },
-        });
+        const response = await fetch(
+          "http://localhost:3000/journal-api/v1/entries",
+          {
+            method: "GET",
+            headers: {
+              Authorization: `Bearer ${token}`,
+              user_id: user.sub,
+            },
+          }
+        );
 
         if (!response.ok) {
           const data = await response.json();
-          throw new Error(data.status || 'Failed to fetch entries');
+          throw new Error(data.status || "Failed to fetch entries");
         }
 
         const data = await response.json();
         setEntries(data.entries);
       } catch (error) {
-        console.error('Error fetching entries:', error.message);
+        console.error("Error fetching entries:", error.message);
       }
     };
 
@@ -73,7 +72,6 @@ const SidebarNav = () => {
   );
 };
 
-
 const SidebarItem = (props) => {
   const text = props.text;
   return (
@@ -85,31 +83,34 @@ const SidebarItem = (props) => {
   );
 };
 
-const MainContent = () => { 
+const MainContent = () => {
   const { user, getAccessTokenSilently } = useAuth0();
 
   const postEntry = async () => {
-      try {
-        const token = await getAccessTokenSilently();
-        const response = await fetch('http://localhost:3000/journal-api/v1/entries', {
-          method: 'POST',
+    try {
+      const token = await getAccessTokenSilently();
+      const response = await fetch(
+        "http://localhost:3000/journal-api/v1/entries",
+        {
+          method: "POST",
           headers: {
             Authorization: `Bearer ${token}`,
             user_id: user.sub,
           },
-          body:{
-            content
-          }
-        });
-
-        if (!response.ok) {
-          const data = await response.json();
-          throw new Error(data.status || 'Failed to fetch entries');
+          body: {
+            content: "This is my first entry ever.",
+          },
         }
-      } catch (error) {
-        console.error('Error posting entries:', error.message);
+      );
+
+      if (!response.ok) {
+        const data = await response.json();
+        throw new Error(data.status || "Failed to fetch entries");
       }
-  }
+    } catch (error) {
+      console.error("Error posting entries:", error.message);
+    }
+  };
 
   return (
     <div className="flex-grow w-1100 bg-white p-6 ml-10 mr-10 rounded-2xl border-2 border-black mt-20 mb-10 flex flex-col justify-between">
@@ -121,10 +122,20 @@ const MainContent = () => {
         <p className="text-gray-800">Hi from main content</p>
       </div>
       <div className="flex items-center justify-center mt-6">
-        <Audio/>
-        <button className="bg-[#FFC000] ml-10 hover:bg-yellow-500 text-white font-bold py-3 px-6 rounded-2xl" onClick={postEntry}>Save</button>
+        <Audio />
+        <button
+          className="bg-[#FFC000] ml-10 hover:bg-yellow-500 text-white font-bold py-3 px-6 rounded-2xl"
+          onClick={postEntry}
+        >
+          Save
+        </button>
 
-        <button className="bg-[#FFC000] ml-5 hover:bg-yellow-500 text-white font-bold py-3 px-6 rounded-2xl" onClick={postEntry}>Delete</button>
+        <button
+          className="bg-[#FFC000] ml-5 hover:bg-yellow-500 text-white font-bold py-3 px-6 rounded-2xl"
+          onClick={postEntry}
+        >
+          Delete
+        </button>
       </div>
     </div>
   );
